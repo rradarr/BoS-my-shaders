c7c63423bc57d7fd789d9518c0abec54ef930d2b
rradarr <radoslaw.rzeczkowski2@gmail.com>
2024-07-22 17:45:33 +0200

    Add exercises on applying matrices inside patterns

--- 6 - Patterns/shaders/BookOfShaders_6_patterns_SimplePS.hlsl
+++ 6 - Patterns/shaders/BookOfShaders_6_patterns_SimplePS.hlsl
@@ -35,6 +35,19 @@ float crossShape(float2 uv, float size, float thickness)
 	return saturate(mask);
 }
 
+float boxShape(float2 uv, float2 size)
+{
+	float mask = 0;
+	
+	uv -= float2(0.5);
+	uv = abs(uv*2);
+	
+	uv = step(uv, size);
+	mask = uv.x * uv.y;
+	
+	return mask;
+}
+
 //  Function from Iñigo Quiles
 //  https://www.shadertoy.com/view/MsS3Wc
 float3 hsb2rgb(float3 c ){
@@ -74,19 +87,87 @@ float3 getColoredGrid(float2 uv, int gridSize, float dotSize)
 	return color;
 }
 
-float getTicTacToe(float2 uv)
+float3 getTicTacToe(float2 uv)
 {
 	const float2 originalUV = uv;
 	const int gridSize = 3;
-	float3 color = 0;
+	float mask = 0;
 	
 	int id = getGridCellId(uv, gridSize);
 	
 	uv = originalUV;
 	uv = frac(uv*gridSize);
-	color = sin(pow(id,9)+uTime) > 0 ? ringShape(uv, 0.2, 0.2) : crossShape(uv, 0.2, 0.15);
+	mask = sin(pow(id,9)+uTime) > 0 ? ringShape(uv, 0.2, 0.2) : crossShape(uv, 0.2, 0.15);
 	
-	return color; 
+	return mask * float3(0.9, 0.1, 0.1); 
+}
+
+float3 getAnimatedPattern(float2 uv)
+{
+	const float2 originalUV = uv;
+	float mask = 0;
+	
+	// Make the space tile 10x in x and y directions.
+	uv = frac(uv*10);
+	
+	// Calculate a fun oscilating value that depends on the x and y position on screen, plus time for animation.
+	float wavyValue = sin(originalUV.x + uTime*0.75 + originalUV.y);
+	
+	// Rotate the tiled spaces around their middle (rotate in place).
+	uv -= float2(0.5);
+	uv = getRotationMat((wavyValue+1)/2 * 1 * PI) * uv;
+	uv += float2(0.5);
+	
+	// Scale the space in the range 0.75-3.75 (lower values make the shapes larger, 0.5 fills entire grid cell).
+	uv -= float2(0.5);
+	uv *= ((wavyValue + 1) / 2) * 3 + 0.75;
+	uv += float2(0.5);
+	// Commenting the uv +- 0.5 (centering the space for scaling) also gives fun results.
+	
+	mask = boxShape(uv, 0.5);
+	
+	return mask;
+}
+
+float3 getLayeredPattern(float2 uv, float gridSize)
+{
+	const float2 originalUV = uv;
+	float mask = 0;
+	
+	// Make the space tile in x and y directions.
+	uv = frac(uv*gridSize);
+	
+	// Draw the center box.
+	mask = boxShape(uv, 0.35);
+
+	// Draw 4 boxes in the corners.
+	mask += boxShape(uv-float2(0.5), 0.25);
+	mask += boxShape(uv-float2(-0.5), 0.25);
+	mask += boxShape(uv-float2(0.5,-0.5), 0.25);
+	mask += boxShape(uv-float2(-0.5,0.5), 0.25);
+	
+	// Draw the X lines from the middle box.
+	mask += crossShape(uv, 0.9, 0.1);
+	
+	// Draw the horizontal and vertical lines with a rotated X.
+	uv -= float2(0.5);
+	uv = getRotationMat(0.25*PI)*uv;
+	uv += float2(0.5);
+	mask += crossShape(uv, 0.5, 0.1);
+	
+	// Satorate the mask values to 0-1 for the subtraction to clear the mask completely.
+	mask = saturate(mask);
+	// Draw the backgdound color rotated box.
+	mask -= boxShape(uv, 0.28);
+	
+	// Finally saturate again to avoid values <1.
+	mask = saturate(mask);
+	
+	// Color foreground and background.
+	float3 color = mask * float3(0.6, 0.7, 0.3);
+	color += (1 - mask) * float3(0.2, 0.6, 0.4);
+	
+	return color;
 }
 
 float4 main(float4 fragCoord : SV_POSITION) : SV_TARGET
@@ -100,9 +181,11 @@ float4 main(float4 fragCoord : SV_POSITION) : SV_TARGET
     color = circleShape(uv, 0.2);
     
     uv = originalUV;
-    color = getColoredGrid(uv, 5, 0.55);
-    color = getTicTacToe(uv) * float3(0.9, 0.1, 0.1);
-    
+    color = getColoredGrid(uv, 50, 0.55);
+    color = getTicTacToe(uv);
+    color = getAnimatedPattern(uv);
+ 	color = getLayeredPattern(uv, 10);
+       
     return float4(color, 1.0f);
 }
 
